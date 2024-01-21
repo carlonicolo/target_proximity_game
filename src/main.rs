49905e0a5989e74cbda5a639eca6d996854c1bad
Carlo <carlo.nicolo@hotmail.com>
2024-01-21 02:21:17 +0100

    utility functions and random func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt::format;
-
+use std::io::{self, stdin};
+use rand::Rng;
 // Define a struct to represent a player
 struct Player {
     name: String,
@@ -18,6 +19,57 @@ impl Printable for Player {
     }
 }
 
+
+/// Define a generic function to get user input
+fn collect_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop{
+        println!("{}", prompt);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        match input.trim().parse() {
+            Ok(value) => return  value,
+            Err(_) => continue,
+            
+        }
+
+    }
+}
+
+/// Define a function to get the players
+fn collect_players() -> Vec<Player> {
+    let mut players: Vec<Player> = Vec::new();
+    let mut num_players = 0;
+    loop {
+        num_players = collect_input::<u32>("How many players (>1) ?");
+        if num_players < 2 {
+            println!("Invalid X no.! Please try again!");
+            continue;
+        } else {
+            break;
+        }
+    }
+    for i in 1..=num_players {
+        let name = collect_input(format!("Player {} name: ", i).as_str());
+        players.push(Player {name, score: 0});
+    }
+    players
+}
+
+
+/// Define a function to get the max number
+fn create_max_range(players: &Vec<Player>) -> u32 {
+    players.len() as u32 * 50
+}
+
+
+// Define a function to generate a random number
+// M-1: via rand library
+fn generate_number(max_range: u32) -> u32 {
+    rand::thread_rng().gen_range(1..max_range)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = generate_number(100);
+    print!("{}",x);
 }
